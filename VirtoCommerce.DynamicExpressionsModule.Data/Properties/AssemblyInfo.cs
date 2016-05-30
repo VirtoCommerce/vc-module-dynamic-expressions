8023b99a37fad4fe1391468b77161a0c6177879e
artem-dudarev <ad@virtoway.com>
2016-05-30 11:57:56 +0200

    Fixed assembly and product titles.

--- VirtoCommerce.DynamicExpressionsModule.Data/Properties/AssemblyInfo.cs
+++ VirtoCommerce.DynamicExpressionsModule.Data/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VirtoCommerce.Marketing.DynamicExpression")]
+[assembly: AssemblyTitle("VirtoCommerce.DynamicExpressionsModule.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VirtoCommerce.Marketing.DynamicExpression")]
+[assembly: AssemblyProduct("VirtoCommerce.DynamicExpressionsModule.Data")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0a737f71-08d4-410e-a0ad-f783d49bdf27")]
 
 // Version information for an assembly consists of the following four values:
 //
--- VirtoCommerce.DynamicExpressionsModule.Web/Properties/AssemblyInfo.cs
+++ VirtoCommerce.DynamicExpressionsModule.Web/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VirtoCommerce.MarketingModule.Web")]
+[assembly: AssemblyTitle("VirtoCommerce.DynamicExpressionsModule.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VirtoCommerce.MarketingModule.Web")]
+[assembly: AssemblyProduct("VirtoCommerce.DynamicExpressionsModule.Web")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,9 +18,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("66b46c89-2523-4918-816b-3efb8e90c623")]
 
 // Version information for an assembly consists of the following four values:
 //
