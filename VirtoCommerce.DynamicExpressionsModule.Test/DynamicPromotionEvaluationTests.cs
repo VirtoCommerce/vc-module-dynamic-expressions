0c50191896c05d6ad80997e63023c2919223b439
Artyom Androsov <aan@virtoway.com>
2017-08-17 14:20:45 +0200

    Moved tests for dynamic conditions from Marketing module

---
+++ VirtoCommerce.DynamicExpressionsModule.Test/DynamicPromotionEvaluationTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using Moq;
+using Newtonsoft.Json;
+using VirtoCommerce.Domain.Common;
+using VirtoCommerce.Domain.Marketing.Model;
+using VirtoCommerce.Domain.Marketing.Services;
+using VirtoCommerce.MarketingModule.Data.Promotions;
+using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.Platform.Core.Serialization;
+using VirtoCommerce.Platform.Data.Serialization;
+using Xunit;
+
+namespace VirtoCommerce.DynamicExpressionsModule.Test
+{
+    public class DynamicPromotionEvaluationTests
+    {
+        protected IExpressionSerializer expressionSerializer = new XmlExpressionSerializer();
+        private ICouponService couponService = new Mock<ICouponService>().Object;
+        private IPromotionUsageService promotionUsageService = new Mock<IPromotionUsageService>().Object;
+
+        [Theory]
+        [MemberData(nameof(EvaluationTestDataGenerator.GetConditions), MemberType = typeof(EvaluationTestDataGenerator))]
+        public void CheckPromotionValid(IConditionExpression[] conditions, IRewardExpression[] rewards, IEvaluationContext context, DynamicPromotionEvaluationResult evaluationResult)
+        {
+            DynamicPromotion dynamicPromotion = GetDynamicPromotion(conditions, rewards);
+
+            var result = dynamicPromotion.EvaluatePromotion(context);
+
+            Assert.Equal(evaluationResult.ValidCount, result.Count(r => r.IsValid));
+            Assert.Equal(evaluationResult.InvalidCount, result.Count(r => !r.IsValid));
+        }
+
+        private DynamicPromotion GetDynamicPromotion(IConditionExpression[] conditions, IRewardExpression[] rewards)
+        {
+            var dynamicPromotion = new DynamicPromotion(expressionSerializer, couponService, promotionUsageService);
+            dynamicPromotion.PredicateSerialized = GetPredicateSerialized(conditions);
+            dynamicPromotion.RewardsSerialized = GetRewardsSerialized(rewards);
+
+            return dynamicPromotion;
+        }
+
+        private string GetPredicateSerialized(IConditionExpression[] conditions)
+        {
+            var predicate = PredicateBuilder.False<IEvaluationContext>();
+            foreach (var expression in conditions.Select(x => x.GetConditionExpression()))
+            {
+                predicate = predicate.Or(expression);
+            }
+
+            return expressionSerializer.SerializeExpression(predicate);
+        }
+
+        private string GetRewardsSerialized(IRewardExpression[] rewards)
+        {
+            var promotionRewards = rewards.SelectMany(r => r.GetRewards()).ToArray();
+            return JsonConvert.SerializeObject(promotionRewards, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+        }
+    }
+}
---
+++ VirtoCommerce.DynamicExpressionsModule.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("VirtoCommerce.DynamicExpressionsModule.Test")]
+[assembly: AssemblyDescription("")]
