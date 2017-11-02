647cb87072938797de0e5fc5af00d81070b2c0c0
artem-dudarev <ad@virtoway.com>
2017-11-02 14:26:17 +0200

    Made GetConditionExpression virtual

--- VirtoCommerce.DynamicExpressionsModule.Data/BlockConditionAndOr.cs
+++ VirtoCommerce.DynamicExpressionsModule.Data/BlockConditionAndOr.cs
@@ -1,30 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using VirtoCommerce.Domain.Common;
-using VirtoCommerce.Domain.Marketing.Model;
 using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.DynamicExpressionsModule.Data
 {
-	public abstract class BlockConditionAndOr : DynamicExpression, IConditionExpression
-	{
-		public bool All { get; set; }
-        //Logical inverse of expression
-        public bool Not { get; set; } = false;
-		#region IConditionExpression Members
+    public abstract class BlockConditionAndOr : DynamicExpression, IConditionExpression
+    {
+        public bool All { get; set; }
 
-		public System.Linq.Expressions.Expression<Func<IEvaluationContext, bool>> GetConditionExpression()
-		{
-			System.Linq.Expressions.Expression<Func<IEvaluationContext, bool>> retVal = null;
-			if (Children != null && Children.Any())
-			{
-				retVal = All ? PredicateBuilder.True<IEvaluationContext>() : PredicateBuilder.False<IEvaluationContext>();
-				foreach (var expression in Children.OfType<IConditionExpression>().Select(x => x.GetConditionExpression()))
-				{
-					retVal = All ? retVal.And(expression) : retVal.Or(expression);
-				}
+        // Logical inverse of expression
+        public bool Not { get; set; } = false;
+
+        public virtual System.Linq.Expressions.Expression<Func<IEvaluationContext, bool>> GetConditionExpression()
+        {
+            System.Linq.Expressions.Expression<Func<IEvaluationContext, bool>> retVal = null;
+            if (Children != null && Children.Any())
+            {
+                retVal = All ? PredicateBuilder.True<IEvaluationContext>() : PredicateBuilder.False<IEvaluationContext>();
+                foreach (var expression in Children.OfType<IConditionExpression>().Select(x => x.GetConditionExpression()))
+                {
+                    retVal = All ? retVal.And(expression) : retVal.Or(expression);
+                }
                 if (Not)
                 {
                     var invokedExpr = System.Linq.Expressions.Expression.Not(System.Linq.Expressions.Expression.Invoke(retVal, retVal.Parameters.Cast<System.Linq.Expressions.Expression>()));
@@ -32,10 +29,7 @@ namespace VirtoCommerce.DynamicExpressionsModule.Data
                 }
             }
             return retVal;
-            
+
         }
-
-        #endregion      
-
     }
 }
