60620b3ffec1d80654914eaf688272a3c3bd2a82
artem-dudarev <ad@virtoway.com>
2017-11-02 14:26:47 +0200

    Made ProductInCategories and ProductInProducts public

--- VirtoCommerce.DynamicExpressionsModule.Data/Promotion/Helpers/PromotionEvaluationContextExtension.cs
+++ VirtoCommerce.DynamicExpressionsModule.Data/Promotion/Helpers/PromotionEvaluationContextExtension.cs
@@ -1,80 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VirtoCommerce.Domain.Marketing.Model;
 
 namespace VirtoCommerce.DynamicExpressionsModule.Data.Promotion
 {
-	public static class PromotionEvaluationContextExtension
-	{
-		#region Dynamic expression evaluation helper methods
-		public static int GetCartItemsQuantity(this PromotionEvaluationContext context, string[] excludingCategoryIds, string[] excludingProductIds)
-		{
-			var retVal = context.CartPromoEntries.ExcludeCategories(excludingCategoryIds)
-								  .ExcludeProducts(excludingProductIds)
-								  .Sum(x => x.Quantity);
-			return retVal;
-		}
+    public static class PromotionEvaluationContextExtension
+    {
+        #region Dynamic expression evaluation helper methods
 
-		public static int GetCartItemsOfCategoryQuantity(this PromotionEvaluationContext context, string categoryId, string[] excludingCategoryIds, string[] excludingProductIds)
-		{
-			var retVal = context.CartPromoEntries.InCategories(new[] { categoryId })
-								  .ExcludeCategories(excludingCategoryIds)
-								  .ExcludeProducts(excludingProductIds)
-								  .Sum(x => x.Quantity);
-			return retVal;
-		}
+        public static int GetCartItemsQuantity(this PromotionEvaluationContext context, string[] excludingCategoryIds, string[] excludingProductIds)
+        {
+            var retVal = context.CartPromoEntries.ExcludeCategories(excludingCategoryIds)
+                                  .ExcludeProducts(excludingProductIds)
+                                  .Sum(x => x.Quantity);
+            return retVal;
+        }
 
-		public static int GetCartItemsOfProductQuantity(this PromotionEvaluationContext context, string productId)
-		{
-			var retVal = context.CartPromoEntries.InProducts(new[] { productId })
-								  .Sum(x => x.Quantity);
-			return retVal;
-		}
+        public static int GetCartItemsOfCategoryQuantity(this PromotionEvaluationContext context, string categoryId, string[] excludingCategoryIds, string[] excludingProductIds)
+        {
+            var retVal = context.CartPromoEntries.InCategories(new[] { categoryId })
+                                  .ExcludeCategories(excludingCategoryIds)
+                                  .ExcludeProducts(excludingProductIds)
+                                  .Sum(x => x.Quantity);
+            return retVal;
+        }
 
-		public static decimal GetCartTotalWithExcludings(this PromotionEvaluationContext context, string[] excludingCategoryIds, string[] excludingProductIds)
-		{
-			var retVal = context.CartPromoEntries.ExcludeCategories(excludingCategoryIds)
-								  .ExcludeProducts(excludingProductIds)
-								  .Sum(x => x.Price * x.Quantity);
+        public static int GetCartItemsOfProductQuantity(this PromotionEvaluationContext context, string productId)
+        {
+            var retVal = context.CartPromoEntries.InProducts(new[] { productId })
+                                  .Sum(x => x.Quantity);
+            return retVal;
+        }
 
-			return retVal;
-		}
+        public static decimal GetCartTotalWithExcludings(this PromotionEvaluationContext context, string[] excludingCategoryIds, string[] excludingProductIds)
+        {
+            var retVal = context.CartPromoEntries.ExcludeCategories(excludingCategoryIds)
+                                  .ExcludeProducts(excludingProductIds)
+                                  .Sum(x => x.Price * x.Quantity);
 
-		public static bool IsItemInCategory(this PromotionEvaluationContext context, string categoryId, string[] excludingCategoryIds, string[] excludingProductIds)
-		{
-			var result = new ProductPromoEntry[] { context.PromoEntry }.InCategories(new[] { categoryId })
-							   .ExcludeCategories(excludingCategoryIds)
-							   .ExcludeProducts(excludingProductIds)
-							   .Any();
+            return retVal;
+        }
+
+        public static bool IsItemInCategory(this PromotionEvaluationContext context, string categoryId, string[] excludingCategoryIds, string[] excludingProductIds)
+        {
+            var result = new ProductPromoEntry[] { context.PromoEntry }.InCategories(new[] { categoryId })
+                               .ExcludeCategories(excludingCategoryIds)
+                               .ExcludeProducts(excludingProductIds)
+                               .Any();
             return result;
-		}
+        }
 
-		public static bool IsItemCodeContains(this PromotionEvaluationContext context, string code)
-		{
-			return new ProductPromoEntry[] { context.PromoEntry }.Where(x => String.Equals(code, x.Code, StringComparison.InvariantCultureIgnoreCase)).Any();
+        public static bool IsItemCodeContains(this PromotionEvaluationContext context, string code)
+        {
+            return new ProductPromoEntry[] { context.PromoEntry }.Where(x => String.Equals(code, x.Code, StringComparison.InvariantCultureIgnoreCase)).Any();
 
-		}
+        }
 
-		public static bool IsAnyLineItemExtendedTotal(this PromotionEvaluationContext context, decimal lineItemTotal, bool isExactly, string[] excludingCategoryIds, string[] excludingProductIds)
-		{
-			if (isExactly)
-				return context.CartPromoEntries.Where(x => x.Price * x.Quantity == lineItemTotal)
-					.ExcludeCategories(excludingCategoryIds)
-					.ExcludeProducts(excludingProductIds)
-					.Any();
-			else
-				return context.CartPromoEntries.Where(x => x.Price * x.Quantity >= lineItemTotal)
-					.ExcludeCategories(excludingCategoryIds)
-					.ExcludeProducts(excludingProductIds)
-					.Any();
-		}
+        public static bool IsAnyLineItemExtendedTotal(this PromotionEvaluationContext context, decimal lineItemTotal, bool isExactly, string[] excludingCategoryIds, string[] excludingProductIds)
+        {
+            if (isExactly)
+                return context.CartPromoEntries.Where(x => x.Price * x.Quantity == lineItemTotal)
+                    .ExcludeCategories(excludingCategoryIds)
+                    .ExcludeProducts(excludingProductIds)
+                    .Any();
+            else
+                return context.CartPromoEntries.Where(x => x.Price * x.Quantity >= lineItemTotal)
+                    .ExcludeCategories(excludingCategoryIds)
+                    .ExcludeProducts(excludingProductIds)
+                    .Any();
+        }
 
-		public static bool IsItemInProduct(this PromotionEvaluationContext context, string productId)
-		{
-			return new ProductPromoEntry[] { context.PromoEntry }.InProducts(new[] { productId }).Any();
+        public static bool IsItemInProduct(this PromotionEvaluationContext context, string productId)
+        {
+            return new ProductPromoEntry[] { context.PromoEntry }.InProducts(new[] { productId }).Any();
         }
 
         public static bool IsItemsInStockQuantity(this PromotionEvaluationContext context, bool isExactly, int quantity)
@@ -84,64 +83,65 @@ namespace VirtoCommerce.DynamicExpressionsModule.Data.Promotion
             else
                 return context.PromoEntry.InStockQuantity >= quantity;
         }
+
         #endregion
 
         #region ProductPromoEntry extensions
+
         public static IEnumerable<ProductPromoEntry> InCategory(this IEnumerable<ProductPromoEntry> entries, string categoryId)
-		{
-			var retVal = entries.InCategories(new[] { categoryId });
-			return retVal;
-		}
+        {
+            var retVal = entries.InCategories(new[] { categoryId });
+            return retVal;
+        }
 
-		public static IEnumerable<ProductPromoEntry> InCategories(this IEnumerable<ProductPromoEntry> entries, string[] categoryIds)
-		{
-			categoryIds = categoryIds.Where(x => x != null).ToArray();
-			var promotionEntries = entries as ProductPromoEntry[] ?? entries.ToArray();
-			return categoryIds.Any() ? promotionEntries.Where(x => IsLineItemInCategories(x, categoryIds)) : promotionEntries;
-		}
+        public static IEnumerable<ProductPromoEntry> InCategories(this IEnumerable<ProductPromoEntry> entries, string[] categoryIds)
+        {
+            categoryIds = categoryIds.Where(x => x != null).ToArray();
+            var promotionEntries = entries as ProductPromoEntry[] ?? entries.ToArray();
+            return categoryIds.Any() ? promotionEntries.Where(x => ProductInCategories(x, categoryIds)) : promotionEntries;
+        }
 
-		public static IEnumerable<ProductPromoEntry> InProduct(this IEnumerable<ProductPromoEntry> entries, string productId)
-		{
-			var retVal = entries.InProducts(new[] { productId });
-			return retVal;
-		}
+        public static IEnumerable<ProductPromoEntry> InProduct(this IEnumerable<ProductPromoEntry> entries, string productId)
+        {
+            var retVal = entries.InProducts(new[] { productId });
+            return retVal;
+        }
 
-		public static IEnumerable<ProductPromoEntry> InProducts(this IEnumerable<ProductPromoEntry> entries, string[] productIds)
-		{
-			productIds = productIds.Where(x => x != null).ToArray();
-			var promotionEntries = entries as IList<ProductPromoEntry> ?? entries.ToList();
-			return productIds.Any() ? promotionEntries.Where(x => IsLineItemInProducts(x, productIds)) : promotionEntries;
-		}
+        public static IEnumerable<ProductPromoEntry> InProducts(this IEnumerable<ProductPromoEntry> entries, string[] productIds)
+        {
+            productIds = productIds.Where(x => x != null).ToArray();
+            var promotionEntries = entries as IList<ProductPromoEntry> ?? entries.ToList();
+            return productIds.Any() ? promotionEntries.Where(x => ProductInProducts(x, productIds)) : promotionEntries;
+        }
 
 
-		public static IEnumerable<ProductPromoEntry> ExcludeCategories(this IEnumerable<ProductPromoEntry> entries, string[] categoryIds)
-		{
-			var retVal = entries.Where(x => !IsLineItemInCategories(x, categoryIds));
-			return retVal;
-		}
+        public static IEnumerable<ProductPromoEntry> ExcludeCategories(this IEnumerable<ProductPromoEntry> entries, string[] categoryIds)
+        {
+            var retVal = entries.Where(x => !ProductInCategories(x, categoryIds));
+            return retVal;
+        }
 
-		public static IEnumerable<ProductPromoEntry> ExcludeProducts(this IEnumerable<ProductPromoEntry> entries, string[] productIds)
-		{
-			var retval = entries.Where(x => !IsLineItemInProducts(x, productIds));
-			return retval;
-		}
+        public static IEnumerable<ProductPromoEntry> ExcludeProducts(this IEnumerable<ProductPromoEntry> entries, string[] productIds)
+        {
+            var retval = entries.Where(x => !ProductInProducts(x, productIds));
+            return retval;
+        }
 
-		private static bool IsLineItemInCategories(ProductPromoEntry entry, IEnumerable<string> categoryIds)
-		{
-			var retVal = categoryIds.Contains(entry.CategoryId);
-			if (!retVal && entry.Outline != null)
-			{
-				retVal = entry.Outline.Split(';', '/', '\\').Intersect(categoryIds).Any();
-			}
-			return retVal;
-		}
+        public static bool ProductInCategories(this ProductPromoEntry entry, ICollection<string> categoryIds)
+        {
+            var result = categoryIds.Contains(entry.CategoryId, StringComparer.OrdinalIgnoreCase);
+            if (!result && entry.Outline != null)
+            {
+                result = entry.Outline.Split(';', '/', '\\').Intersect(categoryIds, StringComparer.OrdinalIgnoreCase).Any();
+            }
+            return result;
+        }
 
-		private static bool IsLineItemInProducts(ProductPromoEntry entry, string[] productIds)
-		{
-			var retVal = productIds.Contains(entry.ProductId, StringComparer.OrdinalIgnoreCase);
-			return retVal;
-		} 
-		#endregion
+        public static bool ProductInProducts(this ProductPromoEntry entry, IEnumerable<string> productIds)
+        {
+            return productIds.Contains(entry.ProductId, StringComparer.OrdinalIgnoreCase);
+        }
 
-	}
+        #endregion
+    }
 }
